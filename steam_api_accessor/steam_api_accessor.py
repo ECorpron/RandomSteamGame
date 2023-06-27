5272257365c2d0e6be3e1ca6cbd4fbe0ff81f865
Eli Corpron <ecorpron@gmail.com>
2023-06-26 17:20:38 -0700

    fixing config file being out of gitignore

--- steam_api_accessor/steam_api_accessor.py
+++ steam_api_accessor/steam_api_accessor.py
@@ -1,10 +1,11 @@
 import credentials.config
-
-import 
+import steam_api_wrapper.steam_api_controller as steam_api
+import steam_api_wrapper.games_manager as games_manager
 
 steam_api_key = credentials.config.steam_api_key
 
 
 def get_profile_games(profile_id):
-    my_api = steam_api.SteamAPIController(api_key=steam_api_key, profile_id=profile_id)
-    my_games_response = my_api.owned_game_request()
+    api_connection = steam_api.SteamAPIController(api_key=steam_api_key, profile_id=profile_id)
+    my_games_response = api_connection.owned_game_request()
+    return games_manager.get_games_dict(my_games_response)
